33697d78a7dee1336bbd9e7f3bc6be8f11c823aa
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-01 06:41:21 +0530

    3 Quest

--- Practice/practice.py
+++ Practice/practice.py
@@ -97,6 +97,61 @@ for i in range(1, len(arr_list)):
     if arr_list[-i] != arr_list[-i-1]:
         print(arr_list[-i-1])
         break'''
+# final_list =[]
+# second_lowest_list =[]
+# second_lowest_list_sort =[]
+
+# for i in range(int(input())):
+#         name = input()
+#         score = float(input())
+#         lis = [name,score]
+#         final_list.append(lis)
+
+# lowest= None
+# second_lowest= None       
+    
+# for i in final_list:
+        
+#     grade = i[1]
+#     if lowest == None or grade <=lowest:
+#             second_lowest = lowest
+#             lowest = grade
+#     elif second_lowest ==None or grade<=second_lowest:
+#             second_lowest = grade
+
+# for j in final_list:
+#        if j[1] == second_lowest:
+#               second_lowest_list.append(j[0])
+              
+
+# second_lowest_list_sorted = sorted(second_lowest_list, key=lambda x: x.lower())
+
+# for m in second_lowest_list_sorted:
+#        print(m)
+
+'''if __name__ == '__main__':
+    alist = []
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        alist.append([name, score])
+second_highest = sorted(set([score for name, score in alist]))[1]
+print('\n'.join(sorted([name for name, score in alist if score == second_highest])))'''
+
+       
+       
+
+
+            
+       
+
+# print(second_lowest)
+
+                
+        
+        
+
+
     
 
 
