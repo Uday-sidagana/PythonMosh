6ad64a64ef280464e4cd989aacbceeea74968ca2
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-05 03:13:33 +0530

    Routes

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello</h1>"
 
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+@app.route('/add/<int:n1>/<int:n2>')
+def add (n1, n2):
+    return f"{n1+n2}"
+
+@app.route('/adds')
+def adds (n1, n2):
+    return f"{n1+n2}"
+
 if __name__ == "__main__":
     app.run(host= '127.0.0.1', port=5000, debug=True)
