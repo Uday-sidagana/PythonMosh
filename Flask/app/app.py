03205c550e48fed9a212a52fac5767ce73d1a236
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-06 06:21:25 +0530

    Mode for accessing or rendering index.html

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -44,13 +44,13 @@ if __name__ == "__main__":
     app.run(host= '127.0.0.1', port=5000, debug=True)'''
 
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello():
-    return "Hello"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port= 5000, debud =True)
