d39e80b9a94117921e600eb2c88324d5b0c06dde
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-06 06:23:00 +0530

    new Port

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -53,4 +53,4 @@ def hello():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port= 5000, debug =True)
+    app.run(host = "0.0.0.0", port= 5500, debug =True)
