fc29cf1f4576d846468e77f44860cd26176aae6a
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-06 06:09:02 +0530

    URL Handling, Methods

--- Flask/app/app.py
+++ Flask/app/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, request, make_response
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello</h1>"
+    response = make_response("Hello")
+    response.status_code = 500
+    response.headers["Content-Type"] = 'applications/octet-stream'
+    #response.headers["Content-Length"] = 20
+    #return "<h1>Hello</h1>", 500
+    return response
 
 @app.route('/greet/<name>')
 def greet(name):
@@ -14,9 +20,25 @@ def greet(name):
 def add (n1, n2):
     return f"{n1+n2}"
 
-@app.route('/adds')
-def adds (n1, n2):
-    return f"{n1+n2}"
+@app.route('/handle_url_params')
+def handle_params():
+    if "greetings" in request.args.keys() and "name" in request.args.keys():
+        # return str(request.args)
+        greetings = request.args['greetings']#Dict key value pair
+        names = request.args.get('name')#get keyword
+        return f"{greetings}, {names}"
+    else:
+        return "Error 404(Params missing)"
+    
+@app.route('/Get_Post_Put_Delete', methods=['GET','POST','PUT','DELETE'])
+def method_test():
+    if request.method == 'GET':
+        return "GET"
+    elif request.method == "POST":
+        return "POST"
+    else:
+        "You'll never see this message"
+
 
 if __name__ == "__main__":
     app.run(host= '127.0.0.1', port=5000, debug=True)
