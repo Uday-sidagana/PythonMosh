f319e43c8c559297d4b8e0e4f52a3d8134b949ef
Uday-sidagana <uday.sidgana@gmail.com>
2024-11-30 23:10:38 +0530

    Changed port

--- Flask/app2/run.py
+++ Flask/app2/run.py
@@ -3,4 +3,4 @@ from app import create_app
 falsk_app = create_app()
 
 if __name__== '__main__':
-    falsk_app.run(host='0.0.0.0', port=5202, debug=True)
+    falsk_app.run(host='0.0.0.0', port=5203, debug=True)
