4aa5dfb31ea5bad81ac7408b1c26544a0ab89418
Uday-sidagana <uday.sidgana@gmail.com>
2024-12-04 23:38:48 +0530

    Form: add new person

--- Flask/app2/routes.py
+++ Flask/app2/routes.py
@@ -7,3 +7,12 @@ def register_routes(app, db):
     def index():
         person = Person.query.all()
         return render_template('index.html', people = person)
+    
+    @app.route('/new_person')
+    def new_person():
+        name = request.form.get('name')
+        age = request.form.get('age')
+        job = request.form.get('job')
+        person = Person.query.all()
+
+        return render_template('index.html', people = person)
