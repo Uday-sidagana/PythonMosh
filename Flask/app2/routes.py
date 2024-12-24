b3ec143e39a39a8f69993987bd89f574a72b3b83
Uday-sidagana <uday.sidgana@gmail.com>
2024-12-24 13:17:32 +0530

    Details added

--- Flask/app2/routes.py
+++ Flask/app2/routes.py
@@ -34,3 +34,10 @@ def register_routes(app, db):
         people = Person.query.all()
         return render_template('index.html', people = people)
     
+    @app.route('/details/<pid>')
+    def details(pid):
+
+        person = Person.query.filter(Person.pid == pid).first()
+
+        return render_template('detail.html', person = person)
+    
