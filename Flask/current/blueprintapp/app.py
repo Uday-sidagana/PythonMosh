78dc77163dd2c6891fa13f6f21fdce0c8bae58a6
Uday-sidagana <uday.sidgana@gmail.com>
2025-01-18 19:01:10 +0530

    create app

--- Flask/current/blueprintapp/app.py
+++ Flask/current/blueprintapp/app.py
@@ -4,3 +4,6 @@ from flask_migrate import Migrate
 from flask import redirect, render_template, url_for
 
 db = SQLAlchemy
+
+def create_app():
+    app = Flask(__name__, template_folder='templates')
