df6260468ca1642ca45f42e9829399ac64ca17b2
Uday-sidagana <uday.sidgana@gmail.com>
2025-01-19 00:20:31 +0530

    db config and init

--- Flask/current/blueprintapp/app.py
+++ Flask/current/blueprintapp/app.py
@@ -7,3 +7,6 @@ db = SQLAlchemy
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./blueprints.db'
+
+    db.init_app(app)
